2a0d6516fddbda50fc99429aaa5b348ecb4ee1d9
Rejoice Angbashim <angbashimrejoice@gmail.com>
2025-08-28 17:19:07 +0100

    Add files via upload

---
+++ openFile.py
@@ -0,0 +1,44 @@
+#writing a file
+file =open('newFile.pdf', 'w')
+file.write("My name is Rejoice. I am a girl.")
+
+#reading a file
+file=open('newFile.pdf', 'r')
+content=file.read()
+print(content)
+
+#appending a file
+file = open('newFile.pdf', 'a')
+file.write("I am 20 years old.")
+
+#input filename
+user=input("filename:")
+
+#reading user input file
+# This code reads a file specified by the user and handles the case where the file does not
+try:
+    file=open(f"{user}", 'r')
+    print(file.read())
+    file.close()
+except FileNotFoundError:
+    print("File not found. Please check the filename and try again.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
